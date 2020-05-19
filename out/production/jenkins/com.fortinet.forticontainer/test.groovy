a5f678384d86212c16fc1d8cd46581e65e0b5ce3
yongyong yuan <yong@yongyongs-MacBook-Pro.local>
2020-05-19 16:51:32 -0700

    debug the steps

--- out/production/jenkins/com.fortinet.forticontainer/test.groovy
+++ out/production/jenkins/com.fortinet.forticontainer/test.groovy
@@ -3,31 +3,35 @@ import groovy.json.JsonBuilder
 import com.fortinet.forticontainer.FortiCSForJenkins
 import com.fortinet.forticontainer.HttpUploadFile
 
-// @NonCPS
-// def Boolean uploadImageTesting(String jobId,String imageName) {
-//     def ctrlHost = "http://internal-fortics-controller-next-1063450219.us-east-1.elb.amazonaws.com";
-//     def controllerToken = "52677600474AFBAB4BD30EEE9D7B6D28"
-//     def sout = new StringBuilder(), serr = new StringBuilder()
+@NonCPS
+def Boolean uploadImageTesting(String jobId,String imageName) {
+    def save = "/var/lib/jenkins/workspace/Test@libs/forticasb-shared-library/src/com/fortinet/forticontainer/saveImage.sh ${imageName} ${tempTarFile}".execute();
+    save.consumeProcessOutput(sout, serr);
+    save.waitForOrKill(1000);
+    println("sout : ${sout}, serr : ${serr}")
+    // def ctrlHost = "http://internal-fortics-controller-next-1063450219.us-east-1.elb.amazonaws.com";
+    // def controllerToken = "52677600474AFBAB4BD30EEE9D7B6D28"
+    // def sout = new StringBuilder(), serr = new StringBuilder()
 
-//     def tempTarFile = "testing:latest"
-//     def save = "docker save ${imageName} -o /tmp/${tempTarFile}.tar ".execute();
+    // def tempTarFile = "testing:latest"
+    // def save = "docker save ${imageName} -o /tmp/${tempTarFile}.tar ".execute();
     
-//     save.consumeProcessOutput(sout, serr);
-//     save.waitForOrKill(1000);
-//     println("sout : ${sout}, serr : ${serr}")
+    // save.consumeProcessOutput(sout, serr);
+    // save.waitForOrKill(1000);
+    // println("sout : ${sout}, serr : ${serr}")
 
-//     def imageFile = new File("/tmp/${tempTarFile}.tar");
-//     if(!imageFile.exists()){
-//         println("did not fund the file ")
-//         return false;
-//     }
+    // def imageFile = new File("/tmp/${tempTarFile}.tar");
+    // if(!imageFile.exists()){
+    //     println("did not fund the file ")
+    //     return false;
+    // }
 
-//     def uploadFile = new HttpUploadFile(ctrlHost+"/api/v1/jenkins/image/"+jobId,controllerToken,tempTarFile);
-//     def result = uploadFile.upload(imageFile);
-//     // def remove = "sudo rm -rf /tmp/tempTarFile:latest.tar".execute()
-//     // remove.waitFor();
-//     return result;
-// }
+    // def uploadFile = new HttpUploadFile(ctrlHost+"/api/v1/jenkins/image/"+jobId,controllerToken,tempTarFile);
+    // def result = uploadFile.upload(imageFile);
+    // // def remove = "sudo rm -rf /tmp/tempTarFile:latest.tar".execute()
+    // // remove.waitFor();
+    // return result;
+}
 
 node {
 
@@ -80,21 +84,22 @@ timestamps{
             println("the job id is ${jobId}");
            
             println("save 2.1 save docker image ${jobId}");
-            for(String image:jenkins.images){
-                print("uploading the image name: ${image} to jobId : ${jobId}")
-                def tempTarFile = "tempTarFile:latest"
-                try {
-                     def uploadStatus = jenkins.uploadImage(jobId,image);
-                    sh("""
-                        ls /tmp
-                       """)
-                } catch(err) {
-                    println("the err while uploading is " + err);
-                }
+            uploadImageTesting(jobId, imageName)
+            // for(String image:jenkins.images){
+            //     print("uploading the image name: ${image} to jobId : ${jobId}")
+            //     def tempTarFile = "tempTarFile:latest"
+            //     try {
+            //          def uploadStatus = jenkins.uploadImage(jobId,image);
+            //         sh("""
+            //             ls /tmp
+            //            """)
+            //     } catch(err) {
+            //         println("the err while uploading is " + err);
+            //     }
                 
-                print("the uploading status is ${uploadStatus}");
-                jenkins.imageResult.put(image, uploadStatus);
-            }
+            //     print("the uploading status is ${uploadStatus}");
+            //     jenkins.imageResult.put(image, uploadStatus);
+            // }
             // boolean status=jenkins.updateJobStatus(jobId,10);
             // print("the status has been update to jobId ${jobId} with status ${status}")
 
