352c66b797233680c3f112f5c1f9494761bd8ff9
yongyong yuan <yong@yongyongs-MacBook-Pro.local>
2020-05-19 23:59:13 -0700

    debug the steps

--- src/com/fortinet/forticontainer/saveImage.sh
+++ src/com/fortinet/forticontainer/saveImage.sh
@@ -2,5 +2,5 @@
 
 #docker save $1 -o /tmp/$2.tar
 
-result = $(curl curl --location --request POST 'http://internal-fortics-controller-next-1063450219.us-east-1.elb.amazonaws.com//api/v1/jenkins/image/12512464169340932' --header 'Content-Type: multipart/form-data' --header 'x-controller-token: 52677600474AFBAB4BD30EEE9D7B6D28' --form 'file=@/Users/yong/Downloads/tempImage:latest.tar')
+curl --location --request POST 'http://internal-fortics-controller-next-1063450219.us-east-1.elb.amazonaws.com//api/v1/jenkins/image/12512464169340932' --header 'Content-Type: multipart/form-data' --header 'x-controller-token: 52677600474AFBAB4BD30EEE9D7B6D28' --form 'file=@/tmp/tempImage:latest.tar')
 #echo $result
