0d6acf68a8dcce0a47bbbb872c20cb93f396710e
zliu <zliu@fortinet.com>
2020-05-20 17:18:56 -0700

    add jenkins script

--- resources/com.fortinet.forticontainer/test.groovy
+++ resources/com.fortinet.forticontainer/test.groovy
@@ -2,7 +2,6 @@
 import groovy.json.JsonBuilder
 import com.fortinet.forticontainer.FortiCSForJenkins
 
-
 node {
 
     ctrlHost = "http://172.30.154.23:10023";
@@ -10,7 +9,6 @@ node {
     projectName = "${env.JOB_NAME}";
     buildNumber = "${env.BUILD_NUMBER}";
     // def userName = "${env.BUILD_USER_ID}";
-    def imageName = "482025328369.dkr.ecr.us-east-1.amazonaws.com/fortics-controller:next-3";
     controllerToken = "52677600474AFBAB4BD30EEE9D7B6D28"
 
 
@@ -18,40 +16,53 @@ node {
     echo "jenkins hot : " + jenkinsHost
     echo "project name : " + projectName
     echo "build number : " + buildNumber
+    def jenkins = new FortiCSForJenkins();
+    jenkins.ctrlHost=ctrlHost;
+    jenkins.controllerToken=controllerToken;
+    jenkins.jenkinsHost=jenkinsHost;
+    jenkins.projectName=projectName;
+    jenkins.buildNumber=buildNumber;
 
-    try {
 
-        stage('Preparation') { // for display purposes
-            // Get some code from a GitHub repository
-            echo "preparation "
-            sleep 2
-        }
-        stage('Build') {
-            echo "build docker"
-            //imageName.add()  //add the image name
-            sleep 2
-        }
+    timestamps{
+        try {
 
-        stage("image scan") {
-            echo "new jenkins plugin";
-            FortiCSForJenkins jenkinsPlugin = new FortiCSForJenkins(ctrlHost,controllerToken,imageName,jenkinHost,projectName,buildNumber);
-            echo "add image";
-            jenkinsPlugin.imageName=imageName;
-            echo "image scan start";
-            int result= imageScan();
-            echo "${result}";
-            if(result>0){
-                currentBuild.result = 'FAILURE';
-                // exit 1
+            stage('Preparation') { // for display purposes
+                // Get some code from a GitHub repository
+                echo "preparation "
+                sleep 2
             }
+            stage('Build') {
+                echo "build docker"
+                //imageName.add()  //add the image name
+                sleep 2
+                echo "add image";
+
+            }
+
+            stage("image scan") {
+                echo "new jenkins plugin";
+                def imageName = "482025328369.dkr.ecr.us-east-1.amazonaws.com/fortics-controller:next-3";
+
+                jenkins.images.add(imageName)
+                echo("uploading image with name is ${imageName}, job id is ${jobIdTest}")
+                def result = jenkins.imageScan();
+                println("the image scan result is ${result}");
+                println("fail message : "+jenkins.message);
+                println(jenkins.imageResult);
+                if(!result){
+                    currentBuild.result = 'FAILURE';
+                    // exit 1
+                }
+            }
+
+            stage("other processing") {
+                echo "other processing";
+            }
+        } catch(err) {
+
+        }finally {
+
         }
-
-        stage("other processing") {
-            echo "other processing";
-        }
-    } catch(err) {
-
-    }finally {
-
     }
 }
